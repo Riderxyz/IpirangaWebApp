1bb16bb246162881d0f0c82cb9034105988d61a1
DESKTOP-EUCDANG\ifavi <ifavilla@gmail.com>
2017-12-20 18:02:56 -0200

    criando autentica??o do login

--- src/app/Pages/login-page/login-page.component.ts
+++ src/app/Pages/login-page/login-page.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RouterModule, Router } from '@angular/router';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+//credencials:any
+Credencials = { usuario: null,senha:null };
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
-alerta(){
-  alert('asas')
-}
+  alerta() {
+    //alert('asas')
+    //this.router.navigateByUrl('/home')
+    console.log(this.Credencials.usuario)
+  }
 }
