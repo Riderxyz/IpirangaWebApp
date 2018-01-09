e446b9fddbd7290445b3a64736777e2b43b2175a
Iago Fabilla <ifavilla@gmail.com>
2018-01-09 12:48:01 -0200

    Tela de Login em 51%

---
+++ login-page.component.ts
@@ -0,0 +1,26 @@
+import { HeaderComponent } from './../../Components/header/header.component';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CacheSrvService } from '../../Services/CacheSrv/cache-srv.service';
+
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+//credencials:any
+Credencials = { usuario: null,senha:null };
+  constructor(public router: Router, public cacheSrv:CacheSrvService) { }
+
+  ngOnInit() {
+    
+  }
+  Login() {
+    this.cacheSrv.SetTitulo('Vip Especialistas')
+   // this.cacheSrv.TituloObj.Header = 'Omega Max'
+    this.cacheSrv.ChamarFuncao();
+    this.router.navigateByUrl('/home')
+  }  
+}
