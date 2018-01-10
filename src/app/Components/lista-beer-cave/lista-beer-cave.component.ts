caec301f12e413d78404200292e5cf59d3dbdf64
ReuelIBlue <rbarbosa@ibllueconsulting.com.br>
2018-01-10 12:06:33 -0200

    lista de relatorio mocado

--- src/app/Components/lista-beer-cave/lista-beer-cave.component.ts
+++ src/app/Components/lista-beer-cave/lista-beer-cave.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-lista-beer-cave',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaBeerCaveComponent implements OnInit {
 
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' }
+      ]
+    },
+
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' }
+          ]
+        }
+
+      ]
+    }
+  ];
+  options = {};
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
