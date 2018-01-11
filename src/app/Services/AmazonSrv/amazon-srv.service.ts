0a447159e5c2e035188c921e283c24e9c8f70e7a
ReuelIBlue <rbarbosa@ibllueconsulting.com.br>
2018-01-11 15:30:32 -0200

    criando lnk de relatorio para ser consumido

--- src/app/Services/AmazonSrv/amazon-srv.service.ts
+++ src/app/Services/AmazonSrv/amazon-srv.service.ts
@@ -11,10 +11,17 @@ import { CacheSrvService } from '../CacheSrv/cache-srv.service';
 export class AmazonSrvService {
   data: any
   Crypto:any
+  linkRelatorio: string ='https://5t2gsxwp07.execute-api.us-east-1.amazonaws.com/DEV/v1/reportsservices/s3list';
+
   constructor(private http: Http, public cacheSrv: CacheSrvService) { }
 
   private urlLoginService = 'https://or9jz65n7i.execute-api.us-east-1.amazonaws.com/DEV/v1/cmsservices/socialmedia-wcm-soapauth-post';
-  private x_api_key = 'U4exZaLqkX8vQatRYqRxk5r3dHKtYtm82EG5b5uN'
+  private x_api_key = 'U4exZaLqkX8vQatRYqRxk5r3dHKtYtm82EG5b5uN';
+
+  listarRelatorio():Observable<any[]>{
+    return this.http.get(this.linkRelatorio).map((resp)=> resp.json());
+  }
+
   private getHeader() {
     // tslint:disable-next-line:prefer-const
     let headers = new Headers();
