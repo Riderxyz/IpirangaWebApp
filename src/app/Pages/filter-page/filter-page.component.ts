4f929789d15837b7e51f20de77a93616c150c559
ifavilla+1-at-525324176518 <ifavilla@gmail.com>
2018-01-11 22:58:06 -0200

    Filter page em 50%

--- src/app/Pages/filter-page/filter-page.component.ts
+++ src/app/Pages/filter-page/filter-page.component.ts
@@ -6,16 +6,32 @@ import { CacheSrvService } from './../../Services/CacheSrv/cache-srv.service';
   styleUrls: ['./filter-page.component.scss']
 })
 export class FilterPageComponent implements OnInit {
-
-  constructor(public cacheSrv:CacheSrvService) { 
-  
-
-  
+  AWS_DatabaseDropdown: any;
+  AWS_DatabaseDatePicker: any;
+  constructor(public cacheSrv: CacheSrvService) {
+  this.itemDabase()
   }
 
   ngOnInit() {
   }
+  itemDabase() {
+    this.AWS_DatabaseDropdown = [
+      { value: '1', viewValue: 'JetOil' },
+      { value: '2', viewValue: 'Padaria' },
+      { value: '3', viewValue: 'Beercave' }
+    ]
 
+    this.AWS_DatabaseDatePicker = [
+      { titulo:'1. Quantidade de Usuario',pickerName: "picker1", pickerID: "#Picker1" },
+      { titulo:'2. Quantidade de publicações por usuario',pickerName: "picker1", pickerID: "#Picker1" },
+      { titulo:'3. Quantidade de publicações por perfil',pickerName: "picker1", pickerID: "#Picker1" },
+      { titulo:'4. Quantidade de visualizações por perfil',pickerName: "picker1", pickerID: "#Picker1" },
+      { titulo:'5. Quantidade de curtidas por publicação',pickerName: "picker1", pickerID: "#Picker1" },
+      { titulo:'6. Quantidade de videos assistidos por dia e faixa de horario',pickerName: "picker1", pickerID: "#Picker1" },
+      { titulo:'7. Quantidade de visualizações por video',pickerName: "picker1", pickerID: "#Picker1" },
+      { titulo:'8. Quantidade geral de acessos, independente da interação',pickerName: "picker1", pickerID: "#Picker1" },
+      { titulo:'9. Quantidade de interações por dia e faixa de horario deas publicações',pickerName: "picker1", pickerID: "#Picker1" },
 
-
+    ]
+  }
 }
