119340a61fdbc4ae1c0f158ae586bf201bdf4b46
Unknown <ifavilla@gmail.com>
2018-01-17 14:15:08 -0200

    condiões de filtros adicionadas

--- src/app/Pages/filter-page/filter-page.component.ts
+++ src/app/Pages/filter-page/filter-page.component.ts
@@ -6,18 +6,18 @@ import { DatePickerComponent } from 'ng2-date-picker';
   selector: 'app-filter-page',
   templateUrl: './filter-page.component.html',
   styleUrls: ['./filter-page.component.scss'],
-  
+
 })
 
 export class FilterPageComponent implements OnInit {
   AWS_DatabaseApps: any;
   AWS_DatabaseFilters: any;
-  AWS_DatabasePerfil:any;
-  AWS_DatabaseAction:any;
+  AWS_DatabasePerfil: any;
+  AWS_DatabaseAction: any;
   Date_Picker_Model1 = { data1: null, data2: null, data3: null, data4: null, data5: null, data6: null, data7: null, data8: null, data9: null, }
   Date_Picker_Model2 = { data1: null, data2: null, data3: null, data4: null, data5: null, data6: null, data7: null, data8: null, data9: null, }
   model: any
-
+  Logado: boolean = false
   @ViewChild('dayPicker') DatePicker_de_Inicio: DatePickerComponent;
   @ViewChild('dayPicker2') DatePicker_de_Fim: DatePickerComponent;
 
@@ -25,7 +25,7 @@ export class FilterPageComponent implements OnInit {
   constructor(public cacheSrv: CacheSrvService) {
     //dateAdapter.setLocale('en-in')
     this.itemDabase()
-    }
+  }
 
   ngOnInit() {
   }
@@ -40,58 +40,94 @@ export class FilterPageComponent implements OnInit {
         titulo: '1. Quantidade de Usuario',
         periodo1: this.Date_Picker_Model1.data1,
         periodo2: this.Date_Picker_Model2.data1,
-        pickerID: "#Picker1",
+//Dropdowns control
         Seta: null,
+        MostrarData: true,
+        MostrarUsuario: true,
+        MostrarAcao: true,
+        MostrarPerfil: true,
       },
 
       {
         titulo: '2. Quantidade de publicações por usuario',
         periodo1: this.Date_Picker_Model1.data2,
         periodo2: this.Date_Picker_Model2.data2,
-        pickerID: "#Picker1"
+//Dropdowns control
+        MostrarData: true,
+        MostrarUsuario: true,
+        MostrarAcao: true,
+        MostrarPerfil: true,
       },
 
       {
         titulo: '3. Quantidade de publicações por perfil',
         periodo1: this.Date_Picker_Model1.data3,
         periodo2: this.Date_Picker_Model2.data3,
-        pickerID: "#Picker1"
+//Dropdowns control
+        MostrarData: false,
+        MostrarUsuario: false,
+        MostrarAcao: false,
+        MostrarPerfil: true,
       },
       {
         titulo: '4. Quantidade de visualizações por perfil',
         periodo1: this.Date_Picker_Model1.data4,
         periodo2: this.Date_Picker_Model2.data4,
-        pickerID: "#Picker1"
+//Dropdowns control
+        MostrarData: false,
+        MostrarUsuario: false,
+        MostrarAcao: false,
+        MostrarPerfil: true,
       },
       {
         titulo: '5. Quantidade de curtidas por publicação',
         periodo1: this.Date_Picker_Model1.data5,
         periodo2: this.Date_Picker_Model2.data5,
-        pickerID: "#Picker1"
+//Dropdowns control
+        MostrarData: false,
+        MostrarUsuario: false,
+        MostrarAcao: false,
+        MostrarPerfil: false,
       },
       {
         titulo: '6. Quantidade de videos assistidos por dia e faixa de horario',
         periodo1: this.Date_Picker_Model1.data6,
         periodo2: this.Date_Picker_Model2.data6,
-        pickerID: "#Picker1"
+//Dropdowns control
+        MostrarData: false,
+        MostrarUsuario: false,
+        MostrarAcao: false,
+        MostrarPerfil: false,
       },
       {
         titulo: '7. Quantidade de visualizações por video',
         periodo1: this.Date_Picker_Model1.data7,
         periodo2: this.Date_Picker_Model2.data7,
-        pickerID: "#Picker1"
+//Dropdowns control
+        MostrarData: false,
+        MostrarUsuario: false,
+        MostrarAcao: false,
+        MostrarPerfil: false,
       },
       {
         titulo: '8. Quantidade geral de acessos, independente da interação',
         periodo1: this.Date_Picker_Model1.data8,
         periodo2: this.Date_Picker_Model2.data8,
-        pickerID: "#Picker1"
+//Dropdowns control
+        MostrarData: false,
+        MostrarUsuario: false,
+        MostrarAcao: false,
+        MostrarPerfil: false,
       },
       {
         titulo: '9. Quantidade de interações por dia e faixa de horario deas publicações',
         periodo1: this.Date_Picker_Model1.data9,
         periodo2: this.Date_Picker_Model2.data9,
-        pickerID: "#Picker1"
+//Dropdowns control
+        MostrarData: false,
+        MostrarUsuario: false,
+        MostrarAcao: false,
+        MostrarPerfil: false,
       },
 
     ]
@@ -124,23 +160,23 @@ export class FilterPageComponent implements OnInit {
   open(date) {
     console.log('SENDO CLICADO!', date)
     if (date == 1) {
-      this.DatePicker_de_Inicio.api.open();  
+      this.DatePicker_de_Inicio.api.open();
     }
     if (date == 2) {
       this.DatePicker_de_Fim.api.open()
     }
-    
+
   }
 
-  teste(item){
+  teste(item) {
     console.log(item)
     this.model = item.viewValue
     console.log(this.model)
-}
+  }
   Ativar(item) {
     console.log(item.value)
     console.log(item.periodo1._d)
     console.log(/*this.Date_Picker_Model*/)
   }
-  
+
 }
